6fab11cb0766f87d40c25d55d70443961054410b
HyperiXQ <goluchm22@gmail.com>
2023-04-02 16:00:05 +0200

    0.4.2

--- main.js
+++ main.js
@@ -1,25 +1,20 @@
 let modal = document.getElementById("myModal");
 let modal2 = document.getElementById("myModal2");
 
-// Get the button that opens the modal
 let btn = document.getElementById("rozwiazanie_btn");
 let btn2 = document.getElementById("egzamin_btn");
 
-// Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
 let span2 = document.getElementsByClassName("close2")[0];
 
-// When the user clicks the button, open the modal 
 btn.onclick = function () {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
@@ -30,12 +25,10 @@ btn2.onclick = function () {
     modal2.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span2.onclick = function () {
     modal2.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal2.style.display = "none";
